575ea60cc9c1c949abfae564a38d1010dc717929
JuanUrrea6 <juan-695@hotmail.com>
2016-01-05 22:02:37 -0500

    Total reform.

--- saveGraph.m
+++
@@ -1,6 +0,0 @@
-%Allows the user to save the generated plot.
-function saveGraph(hObject,handles)
-    global PLOT;
-    fileName = uiputfile('*.*','Save Plot As...');
-    imwrite(PLOT,fileName);
-end
