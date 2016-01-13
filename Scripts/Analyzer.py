84e831e47f945519fdfd3d878e4f9df920e2501e
JuanUrrea6 <juan-695@hotmail.com>
2016-01-13 18:15:20 -0500

    Working on the script.

--- Scripts/Analyzer.py
+++ Scripts/Analyzer.py
@@ -2,33 +2,37 @@
 
 #Import the necessary packages.
 import sys
-import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 from scipy.fftpack import ifft, fft, fftfreq
+import theFunctions as func
 
 #Extract the data from the file input.
 theReading = 0;
 with open(sys.argv[1], 'r') as my_file:
     theReading = my_file.read()
 newArray = theReading.split('\n')
-del newArray[-1]
-newArray = np.array(newArray)
-newArray = [float(i) for i in newArray]
+#Only execute if the last data has a \n
+del newArray[-1];
+newArray = np.array(newArray);
+newArray = [float(i) for i in newArray];
 
 #newArray contains the input signal given by the user, defined by floats.
 #Some useful info about it.
 theLength = len(newArray);
 
-#Finding the X value corresponding to the Y maximum value.
-#This function will allow the program to locate maximums.
-def find_max(X,Y):
-    the_max = max(Y)
-    long = len(X)
-    position = 0
-    for i in range (long):
-        actual_Y = Y[i]
-        if (actual_Y == the_max):
-            position = i
-    answer = X[position]
-    return abs(answer)
+#The user must decie the number of partitions in Gaussian bells that will be
+#done to the data array, as well as the sigma for the bell.
+
+numDiv = input("Number of partitions:");
+theSigma = input("Sigma of Gaussian:");
+numDiv = int(numDiv);
+theSigma = float(theSigma);
+
+#Slidings that must be done with the array and the bells.
+thePos = func.divide(newArray,numDiv);
+#Array that contains all the bell arrays.
+theBells = [func.createBell(newArray, thePos[i],theSigma)*newArray for i in range(numDiv)];
+plt.plot(theBells[0])
+plt.show()
--- Scripts/theFunctions.py
+++ Scripts/theFunctions.py
@@ -1,15 +1,28 @@
 #! /usr/bin python3.4
 
-%pylab inline
+import numpy as np
+import scipy as sp
+import matplotlib.mlab as mlab
 
 def divide(data,numDiv):
     length = len(data)
-    slideThis = length/nums
-    thePos = array([int(np.round(slideThis*i,0)) for i in range(numDiv)])
+    slideThis = length/numDiv
+    thePos = np.array([int(np.round(slideThis*i,0)) for i in range(numDiv)])
     return thePos
 
 def createBell(data,slide,sigma):
     mean = 0
-    theX = linspace(0,len(data),len(data))
-    theBell = normpdf(theX-slide,mean,sigma)
+    theX = np.linspace(0,len(data),len(data))
+    theBell = mlab.normpdf(theX-slide,mean,sigma)
     return theBell
+
+def find_max(X,Y):
+    the_max = max(Y)
+    long = len(X)
+    position = 0
+    for i in range (long):
+        actual_Y = Y[i]
+        if (actual_Y == the_max):
+            position = i
+    answer = X[position]
+    return abs(answer)
